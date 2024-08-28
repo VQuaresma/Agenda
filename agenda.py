27c12b4af2e7d0e068c144f8b683552a3740678d
Vitor Quaresma <vitorquaresmadasilva@gmail.com>
2024-08-28 01:02:08 -0300

    Update
    Atualização para poder trocar dados já cadastrados por outros!

--- agenda.py
+++ agenda.py
@@ -1,6 +1,7 @@
 import sqlite3
 from sqlite3 import Error
 import os
+from time import sleep
 
 res =0
 
@@ -22,15 +23,16 @@ def menu():
 3 - Consultar por ID
 4 - Consultar por nome
 5 - Mostrar todos cadastros
- - Update de cadastro
-6 - Sair """)
+6 - Update de cadastro
+7 - Sair """)
     res = int(input("Digite uma das opções acima: "))
     return res
 
 def saida():
     
-    res = int(input("""6- Sair
-7 - Menu"""))
+    res = int(input("""7 - Sair
+8 - Menu
+"""))
     return res
 
 def inserir(conexao):
@@ -54,6 +56,8 @@ def inserir(conexao):
         
     except Error as ex:
         print(ex)
+    finally:
+        c.close()
 
 def DeletarDados(conexao):
 
@@ -67,26 +71,38 @@ def DeletarDados(conexao):
         print("Registro deletado")
     except Error as ex:
         print(ex)
+    finally:
+        c.close()
 
 def ConsultarId(conexao):
+    try:
+        InputId = int(input('Digite o numero do Id que desejas consultar: '))
+        ConsultaIdSql = f'SELECT * FROM tb_contatos WHERE N_IDCONTATO ={InputId} '
 
-    InputId = int(input('Digite o numero do Id que desejas consultar: '))
-    ConsultaIdSql = f'SELECT * FROM tb_contatos WHERE N_IDCONTATO ={InputId} '
+        c=conexao.cursor()
+        c.execute(ConsultaIdSql)
+        resultado=c.fetchall()
+        return resultado
+    except Error as ex:
+        print(ex)
+    finally:
+        c.close()
 
-    c=conexao.cursor()
-    c.execute(ConsultaIdSql)
-    resultado=c.fetchall()
-    return resultado
 
 def ConsultarNome(conexao):
+    try:
+        InputNome = str(input('Digite o nome que desejas consultar: '))
+        ConsultaNomeSql = f"""SELECT * FROM tb_contatos WHERE T_NOMECONTATO ='{InputNome}' """
 
-    InputNome = str(input('Digite o nome que desejas consultar: '))
-    ConsultaNomeSql = f"""SELECT * FROM tb_contatos WHERE T_NOMECONTATO ='{InputNome}' """
+        c=conexao.cursor()
+        c.execute(ConsultaNomeSql)
+        resultado=c.fetchall()
+        return resultado
+    except Error as ex:
+        print(ex)
+    finally:
+        c.close()
 
-    c=conexao.cursor()
-    c.execute(ConsultaNomeSql)
-    resultado=c.fetchall()
-    return resultado
 
 def MostrarTodos(conexao):
     MostrarSql ='SELECT * FROM tb_contatos'
@@ -96,13 +112,69 @@ def MostrarTodos(conexao):
 
         cadastros =  c.fetchall()
         c.close()
-        return cadastros
+        pos = 1
+        for cadastro in cadastros:
+            print(f"Índice: {pos}, ID: {cadastro[0]}, Nome: {cadastro[1]}, Data: {cadastro[2]}, E-mail: {cadastro[3]}")
+            pos+=1
+
     except Error as ex:
         print(ex)
+    finally:
+        c.close()
+
+def AtualizarDados(conexao):
+    MostrarTodos(vcon)
+    try:
+        c = conexao.cursor()
+
+        sel = int(input("Selecione o id do cadastro que você deseja alterar: "))
+        c.execute(f'SELECT * FROM tb_contatos WHERE N_IDCONTATO ={sel} ')
+
+        resultado = c.fetchall()
+
+        os.system("cls")
+
+        print(f"ID: {resultado[0][0]}, Nome: {resultado[0][1]}, Data: {resultado[0][2]}, E-mail: {resultado[0][3]}")
+        opcoes = {
+        1: "N_IDCONTATO",
+        2: "T_NOMECONTATO",
+        3: "T_TELEFONECONTATO",
+        4: "T_EMAILCONTATO"
+    }
+        UpdateRes = int(input("""Selecione o conteudo que deseja alterar: 
+    1- ID
+    2- Nome
+    3- Telefone
+    4- Email
+    """))
+        os.system("cls")
+        
+        
+        DadoNovo = input("Digite o novo valor que ira ser atribuido: ")
+
+        AtualizarSql=F"UPDATE tb_contatos SET {opcoes[UpdateRes]} = '{DadoNovo}' WHERE N_IDCONTATO ={sel}"
+        c.execute(AtualizarSql)
+        conexao.commit()
+        os.system("cls")
+
+        
+        cont = 1
+        while cont!=4:
+            print('.'*cont)
+            cont+=1
+            sleep(0.5)
+            os.system("cls")
+        print("Cadastro atualizado")
+        sleep(2)
+        
+    except Error as ex:
+        print(ex)
+    finally:
+        c.close()
+        
 
 
-
-while res!=6:
+while res!=7:
     menu()
     if res==1:
         os.system("cls")
@@ -122,10 +194,14 @@ while res!=6:
         res = saida()
     elif res==5:
         os.system("cls")
-        print(MostrarTodos(vcon))
+        MostrarTodos(vcon)
         res = saida()
     elif res==6:
+        AtualizarDados(vcon)
+    elif res==7:
         break
+    else:
+        print("Opção inválida, tente novamente! ")
 os.system("cls")
 vcon.close()
 print("progama finalizado")
